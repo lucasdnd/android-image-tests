44b13981714fd4893c6bd1803eee73e796aa568b
Tulio <tulio@doubleleft.com>
2014-10-29 15:10:01 -0200

    Basic camera, preview and drawing stuff

--- android-image-tests/src/com/lucasdnd/androidimagetests/MainActivity.java
+++ android-image-tests/src/com/lucasdnd/androidimagetests/MainActivity.java
@@ -1,18 +1,41 @@
 package com.lucasdnd.androidimagetests;
 
 import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
-
+	
+	private Camera mCamera;
+    private CameraPreview mPreview;
+    
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    	// Create an instance of Camera
+        mCamera = getCameraInstance();
+    
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+	}
+	
+	/** A safe way to get an instance of the Camera object. */
+	public static Camera getCameraInstance(){
+	    Camera c = null;
+	    try {
+	        c = Camera.open(); // attempt to get a Camera instance
+	    }
+	    catch (Exception e){
+	        // Camera is not available (in use or does not exist)
+	    }
+	    return c; // returns null if camera is unavailable
 	}
 
 	@Override
